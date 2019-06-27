07d03a74a698174e70a62590b0cc4503140e7619
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2019-06-27 15:29:28 +0200

    Mark Lint stage unstable on style violation

--- vars/runClangFormat.groovy
+++ vars/runClangFormat.groovy
@@ -9,12 +9,15 @@ def call(config) {
         ])
         sh '''
             chmod +x clang-format-diff.py
-            ./clang-format-diff.py -p1 < git_diff.txt > clang-format-diff.txt || true
+            ./clang-format-diff.py -p1 < git_diff.txt > clang-format-diff.txt || touch style-violation-found
         '''
         stash([
             includes: 'clang-format-diff.txt',
             name: 'clang-format-result',
         ])
         archiveArtifacts('clang-format-diff.txt')
+        if (fileExists('style-violation-found')) {
+            unstable(message: "Style violation found")
+        }
     }
 }
