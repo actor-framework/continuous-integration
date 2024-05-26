709eb307ebb9943238290efdd531b41f400c94b0
Dominik Charousset <dominik@charousset.de>
2024-05-26 09:02:16 +0200

    Update main branch name

--- vars/getSources.groovy
+++ vars/getSources.groovy
@@ -8,13 +8,13 @@ def call(Map config) {
         checkout scm
         // Generate "git_diff.txt "and optionally "release.txt" for later stages.
         sh """
-            if [ "${env.GIT_BRANCH}" == master ] ; then
-                # on master, we simply compare to the last commit
+            if [ "${env.GIT_BRANCH}" == main ] ; then
+                # on main, we simply compare to the last commit
                 git diff -U0 --no-color HEAD^ > git_diff.txt
             else
                 # in branches, we diff to the merge-base, because Jenkins might not see each commit individually
-                git fetch --no-tags ${env.GIT_URL} +refs/heads/master:refs/remotes/origin/master
-                git diff -U0 --no-color \$(git merge-base origin/master HEAD) > git_diff.txt
+                git fetch --no-tags ${env.GIT_URL} +refs/heads/main:refs/remotes/origin/main
+                git diff -U0 --no-color \$(git merge-base origin/main HEAD) > git_diff.txt
             fi
             if [ -f scripts/get-release-version.sh ] ; then
               sh scripts/get-release-version.sh
